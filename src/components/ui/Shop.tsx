8f3464aa2008aaed821f6226a458719b6487db79
Danil Goncharov <dg444k@gmail.com>
2024-04-01 20:22:14 +0300

    -add loader

--- src/components/ui/Shop.tsx
+++ src/components/ui/Shop.tsx
@@ -4,21 +4,23 @@ import { Categories, ProductType } from '../../types/ProductType'
 import ListProduct from './ListProduct'
 
 const Shop: React.FC = () => {
-	const [products, setProducts] = useState<ProductType[]>([]);
-	const [filterProducts, setFilterProducts] = useState<ProductType[]>(products);
+	const [products, setProducts] = useState<ProductType[]>([])
+	const [filterProducts, setFilterProducts] = useState<ProductType[]>(products)
+	const [isLoader, setIsLoader] = useState<boolean>(true)
 
 	useEffect(() => {
 		fetch('https://fakestoreapi.com/products')
-			.then(async (response) => {
+			.then(async response => {
 				if (!response.ok) {
 					throw new Error('Bad request! Try again')
 				}
-				
+
 				return await response.json()
 			})
 			.then(res => {
-				setProducts(res);
-				setFilterProducts(res);
+				setProducts(res)
+				setFilterProducts(res)
+				setIsLoader(false)
 			})
 	}, [])
 
@@ -29,31 +31,59 @@ const Shop: React.FC = () => {
 				<div className='filters-shop'>
 					<button
 						className='btn-filter filter-clothes'
-						onClick={() => setFilterProducts(() => getFilterProducts(products, Categories.MEN_CLOTHES))}
+						onClick={() =>
+							setFilterProducts(() =>
+								getFilterProducts(products, Categories.MEN_CLOTHES)
+							)
+						}
 					>
 						Men's clothes
 					</button>
 					<button
 						className='btn-filter filter-clothes'
-						onClick={() => setFilterProducts(() => getFilterProducts(products, Categories.WOMEN_CLOTHES))}
+						onClick={() =>
+							setFilterProducts(() =>
+								getFilterProducts(products, Categories.WOMEN_CLOTHES)
+							)
+						}
 					>
 						Women's clothes
 					</button>
 					<button
 						className='btn-filter filter-jewelry'
-						onClick={() => setFilterProducts(() => getFilterProducts(products, Categories.JEWELRY))}
+						onClick={() =>
+							setFilterProducts(() =>
+								getFilterProducts(products, Categories.JEWELRY)
+							)
+						}
 					>
 						Jewelry
 					</button>
 					<button
 						className='btn-filter filter-technics'
-						onClick={() => setFilterProducts(() => getFilterProducts(products, Categories.ELECTRONICS))}
+						onClick={() =>
+							setFilterProducts(() =>
+								getFilterProducts(products, Categories.ELECTRONICS)
+							)
+						}
 					>
 						Technics
 					</button>
 				</div>
 				<div className='content'>
-					<ListProduct products={filterProducts} />
+					{isLoader ? (
+						<p
+							style={{
+								fontSize: '3rem',
+								fontWeight: 'bold',
+								textAlign: 'center',
+							}}
+						>
+							LOADING...
+						</p>
+					) : (
+						<ListProduct products={filterProducts} />
+					)}
 				</div>
 			</div>
 		</div>
